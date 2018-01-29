6ca8fe4a25147f4b2ff860992b8652bc3c72440c
eliotrhys <eliot@eliotshort.co.uk>
2018-01-29 19:41:24 +0000

    added some style and stuff

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 var CAT_ARRAY = [
-  {name: "Name: Snuffles", favouriteFood: "Favourite food: Pepperoni", pictureURL: "http://www.catster.com/wp-content/uploads/2017/06/small-kitten-meowing.jpg"}
+  {name: "Name: Snuffles", favouriteFood: "Favourite food: Pepperoni", pictureURL: "http://www.catster.com/wp-content/uploads/2017/06/small-kitten-meowing.jpg"}, {name: "Name: Boba", favouriteFood: "Favourite food: Sock fluff", pictureURL: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"}, {name: "Name: Barnaby", favouriteFood: "Favourite food: Tuna", pictureURL: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"}, {name: "Name: Max", favouriteFood: "Favourite food: Whiskas", pictureURL: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
 ];
 
 var app = function(){
@@ -26,7 +26,6 @@ function populateCats(array){
     var placeHolderListItem = document.createElement("li");
 
     var catPicture = document.createElement("img");
-    // catPicture.setAttribute = ("src", catPic);
     catPicture.src = catPic;
     catPicture.width = 500;
     placeHolderListItem.appendChild(catPicture);
